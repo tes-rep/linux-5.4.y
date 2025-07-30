931698b93532da274d7bb4af2c8ae54177f83dcb
hj-wrt <mincoek76@gmail.com>
2025-07-31 03:05:55 +0700

    Update meson-gx-socinfo.c

--- drivers/soc/amlogic/meson-gx-socinfo.c
+++ drivers/soc/amlogic/meson-gx-socinfo.c
@@ -41,6 +41,7 @@ static const struct meson_gx_soc_id {
 	{ "G12B", 0x29 },
 	{ "SM1", 0x2b },
 	{ "A1", 0x2c },
+        { "SC2", 0x32 },
 };
 
 static const struct meson_gx_package_id {
@@ -71,6 +72,7 @@ static const struct meson_gx_package_id {
 	{ "S905X3", 0x2b, 0x5, 0xf },
 	{ "S905D3", 0x2b, 0xb0, 0xf0 },
 	{ "A113L", 0x2c, 0x0, 0xf8 },
+        { "S905X4", 0x32, 0x2, 0xff },
 };
 
 static inline unsigned int socinfo_to_major(u32 socinfo)
