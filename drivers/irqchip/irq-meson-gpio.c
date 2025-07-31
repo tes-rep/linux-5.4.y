7da1b571f8c42f713083919f43c3af30a6e5db6a
hj-wrt <mincoek76@gmail.com>
2025-07-31 23:20:51 +0700

    Update irq-meson-gpio.c

--- drivers/irqchip/irq-meson-gpio.c
+++ drivers/irqchip/irq-meson-gpio.c
@@ -71,8 +71,9 @@ static const struct meson_gpio_irq_params sm1_params = {
 };
 
 static const struct meson_gpio_irq_params sc2_params = {
-	INIT_MESON_S4_COMMON_DATA(87)
+	.nr_hwirq = 87,
 };
+
 
 static const struct of_device_id meson_irq_gpio_matches[] __maybe_unused = {
 	{ .compatible = "amlogic,meson8-gpio-intc", .data = &meson8_params },
